40e2e5c32e9c0a92cbad7a486bc98d77cf641a0b
Eleazar Alfriadez <ealfriadez@gmail.com>
2024-09-18 09:07:36 -0500

    first commit

---
+++ src/main/java/pe/edu/unfv/CursoTestingJunitPnApplication.java
@@ -0,0 +1,12 @@
+package pe.edu.unfv;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoTestingJunitPnApplication {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
